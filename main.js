277d662e900724f4e5a67fd8b0e990e40e5a5d84
dilip0418 <sudheer0418@gmail.com>
2023-08-10 02:07:35 +0530

    updated content

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-require('dotenv').config();
+document.addEventListener('DOMContentLoaded', () => {
 
-const apiKey = process.env.WEATHERSTACK_API_KEY;
-const location = document.getElementsByClassName('location')
-const weatherIcon = document.getElementsByClassName('weather-icon');
-const temperatureElement = document.getElementByIdgetElementsByClassName('temperature');
-const conditionsElement = document.getElementsByClassName('weather-desc');
-const locationInput = document.getElementById('location-inp');
-const searchButton = document.getElementById('fetch-btn');
+    const apiKey = 'b67525a4fc4a76145207eaabbaf78866';
+    const location_out = document.getElementById('location')
+    const weatherIcon = document.getElementById('weather-icon');
+    const temperatureElement = document.getElementById('temperature');
+    const conditionsElement = document.getElementById('weather-desc');
 
-// Function to fetch weather data from API
-async function fetchWeatherData(location) {
-    const apiUrl = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${location}`;
+    const locationInput = document.getElementById('location-inp');
+    const searchButton = document.getElementById('fetch-btn');
 
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        return data.current;
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
+    // Function to fetch weather data from API
+    async function fetchWeatherData(location) {
+        const apiUrl = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${location}`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            return data.current;
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
     }
-}
 
-// Function to update UI with weather data
-function updateUI(weatherData) {
-    location.textContent = weatherData.location[name];
-    weatherIcon.src = weatherData.weather_icons[0];
-    temperatureElement.textContent = `${weatherData.temperature}°C`;
-    conditionsElement.textContent = weatherData.weather_descriptions[0];
-}
-
-// Event listener for search button
-searchButton.addEventListener('click', async () => {
-    const location = locationInput.value;
-    if (location) {
-        const weatherData = await fetchWeatherData(location);
-        updateUI(weatherData);
+    // Function to update UI with weather data
+    function updateUI(weatherData) {
+        location_out.textContent = locationInput.textContent;
+        weatherIcon.src = weatherData.weather_icons[0];
+        temperatureElement.textContent = `${weatherData.temperature}°C`;
+        conditionsElement.textContent = weatherData.weather_descriptions[0];
     }
+
+    // Event listener for search button
+    searchButton.addEventListener('click', async () => {
+        const location = locationInput.value;
+        if (location) {
+            const weatherData = await fetchWeatherData(location);
+            console.log(weatherData);
+            updateUI(weatherData);
+        }
+    });
 });
